1da9b0f3ec6f654970ab860dd27cbb7931087c2a
NeoClass2174 <takacs.bence@kkszki.hu>
2024-12-05 19:07:21 +0100

    Véletlen számok

---
+++ test1.py
@@ -0,0 +1,15 @@
+#Készítsen python kódot ami bekér egy egész hőmérséklet értéket
+#és kiírja hogy az adott értéken milyen halmazállapotú a víz.
+
+def homerseklet():
+    homersekletC = int(input("Kérek egy hőmérséklet értéket: "))
+    print(type(homersekletC))
+
+    if homersekletC <= 0:
+        print("Szilárd halmazállapot") #igaz ág
+    elif homersekletC < 100:
+        print("Folyékony halmazállapot.")
+    else:
+        print("Légnemű halmazállapot") #hamis ág
+
+homerseklet()
